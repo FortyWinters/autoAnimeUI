6f4f39b3e07d80b48b2459b616f553769b7552c0
HeasonNn <haisong07@yeah.net>
2024-04-04 00:18:41 +0800

    fix

--- src/pages/video/index.vue
+++ src/pages/video/index.vue
@@ -1,36 +1,47 @@
 <template>
     <el-row :gutter="0">
         <el-col :span="3">
-            <div class="grid-content ep-bg-purple" />
-            <img src="http://127.0.0.1:9999/1.gif" alt="GIF Image" class="leftImage">
+            <div class="grid-content ep-bg-purple">
+                <img :src="gifPath3" alt="GIF Image" class="leftImage">
+            </div>
         </el-col>
         <el-col :span="16">
             <el-row :gutter="1">
                 <div class="topbar">
                     <router-link :to="'../home'" class="link">Home</router-link>
-                    <span>></span>
+                    <span> >> </span>
                     <router-link :to="'../anime?mikan_id=3240'" class="link">Anime</router-link>
-                    <span>></span>
+                    <span> >> </span>
                     <router-link :to="'../video'" class="link">Video</router-link>
                 </div>
             </el-row>
             <el-row :gutter="1">
-                <div class="grid-content ep-bg-purple" />
-                <video controls ref="videoPlaye" autoplay="false" width="800" height="460">
-                    <source :src="videoPath" type="video/mp4">
-                    Your browser does not support the video tag.
-                </video>
+                <div class="grid-content ep-bg-purple">
+                    <video controls autoplay="false" width="800" height="460">
+                        <source :src="videoPath" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
             </el-row>
             <el-row :gutter="1">
-                <div class="grid-content ep-bg-purple" />
-                <div class="subgroup-tab" style="">
-                    <Tab :subgroupArr="subgroupArr" :mikanId="mikanId" />
+                <div class="grid-content ep-bg-purple">
+                    <div class="subgroup-tab" style="">
+                        <Tab :subgroupArr="subgroupArr" :mikanId="mikanId" />
+                    </div>
+                </div>
+            </el-row>
+            <el-row :gutter="1">
+                <div class="grid-content ep-bg-purple">
+                    <!-- <img :src="gifPath4" alt="GIF Image" style="width: 100px; height: 100px;"> -->
+                    <span class="text">BanG(umi) Dream! It's My AutoAnime ! ! ! ! !</span>
+                    <span style="font-size: 25px;"> ✌️🥵✌️</span>
                 </div>
             </el-row>
         </el-col>
         <el-col :span="3">
-            <div class="grid-content ep-bg-purple" />
-            <img src="http://127.0.0.1:9999/1.gif" alt="GIF Image" class="rightImage">
+            <div class="grid-content ep-bg-purple">
+                <img :src=gifPath3 alt="GIF Image" class="rightImage">
+            </div>
         </el-col>
     </el-row>
 </template>
@@ -39,11 +50,12 @@
 import { useRoute } from 'vue-router'
 import { reactive, ref } from 'vue'
 import Tab from '../anime/tab/index.vue'
-
 const $route = useRoute()
 
 let mikanId = $route.query.mikan_id
 let videoPath = ref("http://127.0.0.1:9999/迷宫饭/迷宫饭 - 13 - 喵萌奶茶屋.mp4")
+let gifPath3 = ref("http://127.0.0.1:9999/3.gif")
+let gifPath4 = ref("http://127.0.0.1:9999/4.gif")
 
 let subgroupArr = reactive([
     { subgroup_id: 1, subgroup_name: "字幕组1" },
@@ -51,10 +63,12 @@ let subgroupArr = reactive([
     { subgroup_id: 3, subgroup_name: "字幕组3" },
     { subgroup_id: 4, subgroup_name: "字幕组4" }
 ])
+
 </script>
 
 <style scoped lang="scss">
 .topbar {
+    font-family: Serif;
     margin-left: 0px;
     margin-bottom: 10px;
     height: 15px;
@@ -62,12 +76,14 @@ let subgroupArr = reactive([
 }
 
 .link {
+    font-family: Serif;
     text-decoration: none;
     color: inherit;
     font-weight: 500;
-    font-size: 18px;
+    font-size: 20px;
     margin-left: 6px;
     margin-right: 6px;
+    color: #686868;
 }
 
 .el-row {
@@ -99,5 +115,14 @@ let subgroupArr = reactive([
     width: 100px;
     height: 100px;
     transform: scaleX(-1);
+}
+
+.text {
+    margin-left: 300px;
+    font-family: Serif;
+    font-weight: 100;
+    font-size: 20px;
+    font-style: italic;
+    color: rgb(100, 100, 100);
 }
 </style>@/store/modules/anime
