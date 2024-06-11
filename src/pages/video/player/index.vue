8b8529d52724405f557820e93285a7441e43501c
HeasonNn <haisong07@yeah.net>
2024-06-11 22:04:46 +0800

    fix

--- src/pages/video/player/index.vue
+++ src/pages/video/player/index.vue
@@ -1,7 +1,7 @@
 <template>
-	<div ref="dplayerContainer" class="dplayer-container">
-		<div id="dplayer"></div>
-	</div>
+    <div ref="dplayerContainer" class="dplayer-container">
+        <div id="dplayer"></div>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -9,8 +9,8 @@ import { defineProps, onBeforeUnmount, onMounted, ref } from 'vue';
 import DPlayer from 'dplayer';
 
 const props = defineProps({
-	videoPath: String,
-	subtitlePath: String,
+    videoPath: String,
+    subtitlePath: String,
 });
 
 const videoPath = String(props.videoPath)
@@ -21,62 +21,62 @@ const dplayerContainer = ref<HTMLElement | null>(null);
 const dp = ref<DPlayer | null>(null);
 
 const adjustSubtitleFontSize = () => {
-	if (dp.value && dp.value.video) {
-		const container = dplayerContainer.value;
-		if (container) {
-			const subtitleElement = container.querySelector('.dplayer-subtitle') as HTMLElement;
-			if (subtitleElement) {
-				const videoWidth = dp.value.video.offsetWidth;
-				if (videoWidth) {
-					const newFontSize = `${videoWidth * 0.04}px`;
-					subtitleElement.style.fontSize = newFontSize;
-					console.log(newFontSize);
-				}
-			}
-		}
-	}
+    if (dp.value && dp.value.video) {
+        const container = dplayerContainer.value;
+        if (container) {
+            const subtitleElement = container.querySelector('.dplayer-subtitle') as HTMLElement;
+            if (subtitleElement) {
+                const videoWidth = dp.value.video.offsetWidth;
+                if (videoWidth) {
+                    const newFontSize = `${videoWidth * 0.04}px`;
+                    subtitleElement.style.fontSize = newFontSize;
+                    console.log(newFontSize);
+                }
+            }
+        }
+    }
 };
 
 const initializeDPlayer = () => {
-	dp.value = new DPlayer({
-		container: document.getElementById('dplayer'),
-		screenshot: true,
-		video: {
-			url: videoPath,
-		},
-		subtitle: {
-			url: subtitlePath,
-			fontSize: '30px', // Initial font size
-		},
-	});
+    dp.value = new DPlayer({
+        container: document.getElementById('dplayer'),
+        screenshot: true,
+        video: {
+            url: videoPath,
+        },
+        subtitle: {
+            url: subtitlePath,
+            fontSize: '30px', // Initial font size
+        },
+    });
 };
 
 onMounted(() => {
-	initializeDPlayer();
+    initializeDPlayer();
 
-	if (dplayerContainer.value) {
-		// Utilize ResizeObserver to monitor container size changes
-		const resizeObserver = new ResizeObserver(adjustSubtitleFontSize);
-		resizeObserver.observe(dplayerContainer.value);
+    if (dplayerContainer.value) {
+        // Utilize ResizeObserver to monitor container size changes
+        const resizeObserver = new ResizeObserver(adjustSubtitleFontSize);
+        resizeObserver.observe(dplayerContainer.value);
 
-		// Disconnect the observer when the component is unmounted
-		onBeforeUnmount(() => {
-			resizeObserver.disconnect();
-		});
-	}
+        // Disconnect the observer when the component is unmounted
+        onBeforeUnmount(() => {
+            resizeObserver.disconnect();
+        });
+    }
 });
 
 </script>
 
 <style scoped lang="scss">
 .dplayer-container {
-	width: 100%;
-	max-width: 800px;
-	margin: auto;
+    width: 100%;
+    max-width: 800px;
+    margin: auto;
 }
 
 #dplayer {
-	width: 100%;
-	height: auto;
+    width: 100%;
+    height: auto;
 }
 </style>
